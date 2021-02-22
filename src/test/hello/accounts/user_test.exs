c920dea0dbeb0fe493f3ac8dd8de29c9fbe9036b
Yuri Amami <yuri.u.0425@gmail.com>
2021-02-22 09:44:48 -0500

    revised test cases

--- src/test/hello/accounts/user_test.exs
+++ src/test/hello/accounts/user_test.exs
@@ -1,6 +1,7 @@
 # TODO: write test for changeset
 defmodule Hello.Accounts.UserTest do
   use Hello.DataCase
+
   import Ecto.Repo
 
   alias Hello.Accounts
@@ -15,18 +16,55 @@ defmodule Hello.Accounts.UserTest do
 
     test "should insert data into users table when it has correct length" do
       changeset = User.changeset(%User{}, @valid_attrs)
+
       assert changeset.valid?
     end
 
     test "should return error messages when empty username passed in" do
       changeset = User.changeset(%User{}, %{username: "", password: "password"})
-      assert changeset.errors[:username] != nil
+      error_message = Ecto.Changeset.traverse_errors(changeset, fn {msg, _} -> msg end)
+
+      assert error_message == %{username: ["can't be blank"]}
+      refute changeset.valid?
+    end
+
+    test "should return error messages when username is nil" do
+      changeset = User.changeset(%User{}, %{username: nil, password: "password"})
+      error_message = Ecto.Changeset.traverse_errors(changeset, fn {msg, _} -> msg end)
+
+      assert error_message == %{username: ["can't be blank"]}
+      refute changeset.valid?
+    end
+
+    test "should return error messages when empty password passed in" do
+      changeset = User.changeset(%User{}, %{username: "username", password: ""})
+      error_message = Ecto.Changeset.traverse_errors(changeset, fn {msg, _} -> msg end)
+
+      assert error_message == %{password: ["can't be blank"]}
+      refute changeset.valid?
+    end
+
+    test "should return error messages when password is nil" do
+      changeset = User.changeset(%User{}, %{username: "username", password: nil})
+      error_message = Ecto.Changeset.traverse_errors(changeset, fn {msg, _} -> msg end)
+
+      assert error_message == %{password: ["can't be blank"]}
       refute changeset.valid?
     end
 
     test "should return error messages when shorter username passed in" do
-      changeset = User.changeset(%User{}, %{username: "five", password: "password"})
-      assert changeset.errors[:username] != nil
+      changeset = User.changeset(%User{}, %{username: "five5", password: "password"})
+      error_message = Ecto.Changeset.traverse_errors(changeset, fn {msg, _} -> msg end)
+
+      assert error_message == %{username: ["should be at least %{count} character(s)"]}
+      refute changeset.valid?
+    end
+
+    test "should return error messages when shorter password passed in" do
+      changeset = User.changeset(%User{}, %{username: "username", password: "five5"})
+      error_message = Ecto.Changeset.traverse_errors(changeset, fn {msg, _} -> msg end)
+
+      assert error_message == %{password: ["should be at least %{count} character(s)"]}
       refute changeset.valid?
     end
 
@@ -38,23 +76,22 @@ defmodule Hello.Accounts.UserTest do
           password_confirmation: "invaid_password"
         })
 
-      assert changeset.errors[:password_confirmation] != nil
+      error_message = Ecto.Changeset.traverse_errors(changeset, fn {msg, _} -> msg end)
+
+      assert error_message == %{password_confirmation: ["does not match confirmation"]}
       refute changeset.valid?
     end
 
     test "should return error messages when the user already exists" do
       assert {:ok, %User{}} = Accounts.create_user(@valid_attrs)
 
-      duplicated_user =
-        User.changeset(%User{}, %{
-          username: "username",
-          password: "password",
-          password_confirmation: "password"
-        })
+      {:error, %Ecto.Changeset{} = changeset} = Accounts.create_user(@valid_attrs)
 
-      assert {:error, changeset} = Repo.insert(duplicated_user)
-      assert changeset.errors[:username] != nil
       refute changeset.valid?
+
+      error_message = Ecto.Changeset.traverse_errors(changeset, fn {msg, _} -> msg end)
+
+      assert error_message == %{username: ["has already been taken"]}
     end
   end
 end
